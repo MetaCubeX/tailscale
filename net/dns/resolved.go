bf38f986e7d713577fbc5641767d87634b581f63
wwqgtxx <wwqgtxx@gmail.com>
2026-07-11 20:16:31 +0800

    disable resolved

--- net/dns/resolved.go
+++ net/dns/resolved.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build linux && !android && !ts_omit_resolved
+//go:build linux && !android && ts_enable_resolved
 
 package dns
 
