f7c1558ee37e9fe5368c1008c50e4098e87d055d
wwqgtxx <wwqgtxx@gmail.com>
2026-07-11 21:18:44 +0800

    disable debug eventbus

--- util/eventbus/debughttp_off.go
+++ util/eventbus/debughttp_off.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build ios || android || ts_omit_debugeventbus
+//go:build ios || android || !ts_enable_debugeventbus
 
 package eventbus
 
